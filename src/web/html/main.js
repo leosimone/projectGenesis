7499480624e4437d28963feabf29331a256e98a0
Leonardo Simone <leosimone@gmail.com>
2022-09-20 09:36:16 -0300

    fetch

--- src/web/html/main.js
+++ src/web/html/main.js
@@ -4,65 +4,106 @@ async function handleClick() {
     const url = 'https://private-847f5-ivangenesis.apiary-mock.com/users';
 
 
-    var users = await fetch(url, {
-            method: 'GET',
-        }).then((response) => {response.json()}).then((data) => {
-            const users = data;
-            users.map(function(user){
-                const li = document.createElement('li');
-                const name = document.createElement('h2');
-                const email = document.createElement('span');
 
-                name.innerHTML = `${user.name}`;
-                email.innerHTML = `${user.email}`;
+    const users = fetch('https://private-847f5-ivangenesis.apiary-mock.com/users').then(async (res) => {
+    const response = await res.json()    
+    return response;
+});
 
-                li.appendChild(name);
-                li.appendChild(email);
-                list.appendChild(li);
 
-            });
-        }).catch(function(error){
-            console.log(error)
-        });
+// function getInfo(users){
 
-        ul.appendChild(list);
+//     users.map(function(user){
+//         const li = document.createElement('li');
+//         const name = document.createElement('h2');
+//         const email = document.createElement('span');
 
-        console.log(users);
+//         name.innerHTML = `${user.name}`;
+//         email.innerHTML = `${user.email}`;
+
+//         li.appendChild(name);
+//         li.appendChild(email);
+//         list.appendChild(li);
+
+//     });
+
+// ul.appendChild(list);
+// }
+
+
+    // const fetchTableData = ( => {
+    //     return fetch('https://private-847f5-ivangenesis.apiary-mock.com/users')
+    //       .then(result => result.json())
+    //       .then(data => {
+    //           return data;
+    //       })
+    //   }
+    //   console.log(fetchTableData[0].data);
+  
+    //   export default fetchTableData;
+
+
+    // var users = await fetch(url, {
+    //         method: 'GET',
+    //     }).then((response) => {response.json()}).then((data) => {
+    //         const users = data;
+    //         users.map(function(user){
+    //             const li = document.createElement('li');
+    //             const name = document.createElement('h2');
+    //             const email = document.createElement('span');
+
+    //             name.innerHTML = `${user.name}`;
+    //             email.innerHTML = `${user.email}`;
+
+    //             li.appendChild(name);
+    //             li.appendChild(email);
+    //             list.appendChild(li);
+
+    //         });
+    //     }).catch(function(error){
+    //         console.log(error)
+    //     });
+
+    //     ul.appendChild(list);
+
+    //     console.log(users);
        // return JSON.stringify(users);
     }
 
-// const datatable = document.querySelector('.infoTable')
-// const tbody = datatable.querySelector('tbody')
-// const thead = datatable.querySelector('thead')
 
-// const columns = ['id', 'Name', 'CPF', 'Phone', 'Email'];
+function getInfo(users){
+const datatable = document.querySelector('.infoTable')
+const tbody = datatable.querySelector('tbody')
+const thead = datatable.querySelector('thead')
 
-// const rows = [
-//     { id: 1, name: 'teste', CPF: '04080757247', Phone: '11987654321', email: 'myemail1@test.com.br' },
-//     { id: 2, name: 'My name 2', CPF: '77797584192', Phone: '11987654321', email: 'myemail2@test.com.br' },
-//     { id: 3, name: 'My name 3', CPF: '45486737688', Phone: '11987654321', email: 'myemail1@test.com.br' },
-// ]
+const columns = ['id', 'Name', 'CPF', 'Phone', 'Email'];
 
-// const columnsTable = columns.map(column =>(
-//     `<th>${column}<th>`
-// ));
+const rows = users
 
-// thead.rows.item(0).innerHTML = columnsTable.join('');
+const columnsTable = columns.map(column =>(
+    `<th>${column}<th>`
+));
 
-// const rowsTable = rows.map(row =>(
-//     `
-//     <tr>
-//         <td>${row.name}</td>
-//         <td>${row.CPF}</td>
-//         <td>${row.Phone}</td>
-//         <td>${row.email}</td>
-//     </tr>
-//     `
-// ));
-// tbody.innerHTML = rowsTable.join('');
+thead.rows.item(0).innerHTML = columnsTable.join('');
+
+// for(row of rows){
+const rowsTable = rows.map(row =>(
+    `
+    <tr>
+        <td>${row.name}</td>
+        <td>${row.CPF}</td>
+        <td>${row.Phone}</td>
+        <td>${row.email}</td>
+    </tr>
+    `
+));
+
+// }
+tbody.innerHTML = rowsTable.join('');
+}
 
 const tbody = document.querySelector('tbody');
-const name = document.querySelector('formName');
+const nameUser = document.querySelector('formName');
 const cpf = document.querySelector('formCpf');
 const phone = document.querySelector('formPhone');
 const email = document.querySelector('formEmail');
@@ -71,7 +112,7 @@ const btnSubmit = document.querySelector('btnSubmit');
 let itens = ''; //itens para manipulação das informações do localstorage
 let id; //id que serve para pegar o index do array no localstorage
 
-//abaixo pega os itens da LocalStorage ou retorna um array vazio caso não haja
+//abaixo pega os itens da LocalStorage ou retorna um array vazio caso não haja, mas tem a API
 const getItensLS = () => JSON.parse(localStorage.getItem('lsFunction')) ?? [];
 const setItensLS = () => localStorage.setItem('lsFunction', JSON.stringify(itens));
 
@@ -87,10 +128,10 @@ function loadInformation(){
 loadInformation();
 
 function insertItem(item, index){
-    const tr = document.createElement('tr'); //creação dos td com as informações
+    const tr = document.createElement('tr'); //criação dos td com as informações
     tr.innerHTML = 
     `
-    <td>${item.name}</td>
+    <td>${item.nameUser}</td>
     <td>${item.cpf}</td>
     <td>${item.phone}</td>
     <td>${item.email}</td>
@@ -103,3 +144,32 @@ function insertItem(item, index){
     `
     tbody.appendChild(tr); //carregando para dentro do body para ser apresentado em tela
 }
+
+function excludeItem(index){
+    itens.splice(index, 1) //pega o index, retira um item
+    setItensLS(); //atualiza no LS
+    loadInformation(); //carrega novamente as informações
+}
+
+function editItem(index) {
+
+    insert(true, index);
+  }
+
+function insert(edit = false, index = 0){ //vem como padrao false, caso seja edição virá true
+
+    if(edit){
+        nameUser.value = itens[index].name;
+        cpf.value = itens[index].cpf;
+        phone.value = itens[index].phone;
+        email.value = itens[index].email;
+    }else{
+        nameUser.value = '';
+        cpf.value = '';
+        phone.value = '';
+        email.value = '';
+    }
+
+
+
+} 
