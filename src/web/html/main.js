b4095a3c518258caa4c45ea134a540529125736a
Leonardo <leosimone@gmail.com>
2022-09-20 22:34:55 -0300

    enhancements

--- src/web/html/main.js
+++ src/web/html/main.js
@@ -8,20 +8,49 @@ async function handleClick() {
     return response;
 });
 
-const newTable = document.createElement("table");
-const header = document.createElement("thead");
-const body = document.createElement("tbody");
-const td = document.createElement("td");
+// const newTable = document.createElement("table");
+const thead = document.createElement("thead");
+ const tbody = document.createElement("tbody");
+// const td = document.createElement("td");
 
-td.innerHTML = `${users[0].name}`;
-td.innerHTML = `${users[0].cpf}`;
-td.innerHTML = `${users[0].phone}`;
-td.innerHTML = `${users[0].email}`;
+// td.innerHTML = `${users[0].name}`;
+// td.innerHTML = `${users[0].cpf}`;
+// td.innerHTML = `${users[0].phone}`;
+// td.innerHTML = `${users[0].email}`;
 
-newTable.appendChild(header);
-newTable.appendChild(body);
-newTable.appendChild(td);
-document.getElementById("usersApi").appendChild(newTable);
+// newTable.appendChild(header);
+// newTable.appendChild(body);
+// newTable.appendChild(td);
+// document.getElementById("usersApi").appendChild(newTable);
+
+
+const columns = ['id', 'Name', 'CPF', 'Phone', 'Email'];
+const rows = users
+
+const columnsTable = columns.map(column =>(
+    `<th>${column}<th>`
+));
+
+    // thead.rows.item(0).innerHTML = columnsTable.join('');
+
+ for(row of rows){
+const rowsTable = rows.map(row =>(
+    `
+    <tr>
+        <td>${row.name}</td>
+        <td>${row.CPF}</td>
+        <td>${row.Phone}</td>
+        <td>${row.email}</td>
+    </tr>
+    `
+));
+tbody.innerHTML = rowsTable.join('');
+}
+
+
+
+
+
 
 
 
@@ -66,16 +95,7 @@ function getInfo(){
 }
 // document.onload(getInfo());
 
-    // const fetchTableData = ( => {
-    //     return fetch('https://private-847f5-ivangenesis.apiary-mock.com/users')
-    //       .then(result => result.json())
-    //       .then(data => {
-    //           return data;
-    //       })
-    //   }
-    //   console.log(fetchTableData[0].data);
-  
-    //   export default fetchTableData;
+
 
 
     // var users = await fetch(url, {
@@ -138,101 +158,118 @@ function getInfo(){
 // tbody.innerHTML = rowsTable.join('');
 // }
 
-const tbody = document.querySelector('#tbody');
-const nameUser = document.querySelector('#formName');
-const cpf = document.querySelector('#formCpf');
-const phone = document.querySelector('#formPhone');
-const email = document.querySelector('#formEmail');
-const btnSubmit = document.querySelector('#btnSubmit');
+// ---------------------------------------
+// const tbody = document.querySelector('#tbody');
+// const nameUser = document.querySelector('#formName');
+// const cpf = document.querySelector('#formCpf');
+// const phone = document.querySelector('#formPhone');
+// const email = document.querySelector('#formEmail');
+// const btnSubmit = document.querySelector('#btnSubmit');
 
-let itens = ''; //itens para manipulação das informações do localstorage
-let id; //id que serve para pegar o index do array no localstorage
+// let itens = ''; //itens para manipulação das informações do localstorage
+// let id; //id que serve para pegar o index do array no localstorage
 
-//abaixo pega os itens da LocalStorage ou retorna um array vazio caso não haja, mas tem a API
-const getItensLS = () => JSON.parse(localStorage.getItem('lsFunction'));
-const setItensLS = () => localStorage.setItem('lsFunction', JSON.stringify(itens));
+// //abaixo pega os itens da LocalStorage ou retorna um array vazio caso não haja, mas tem a API
+// const getItensLS = () => JSON.parse(localStorage.getItem('lsFunction'));
+// const setItensLS = () => localStorage.setItem('lsFunction', JSON.stringify(itens));
 
-//função para iniciar com as informações da LS, ou que retornam da API
-function loadInformation(){
-    itens = getItensLS();
-    tbody.innerHTML = ''
-    itens.forEach((item, index)=>{ //for em cada dado para ser criada a linha
-        insertItem(item, index)
-    })
-}
+// // função para iniciar com as informações da LS, ou que retornam da API
+// function loadInformation(){
+//     itens = getItensLS();
+//     tbody.innerHTML = '';
+//     itens.forEach((item, index)=>{ //for em cada dado para ser criada a linha
+//         insertItem(item, index)
+//     })
+// }
 
-loadInformation();
+// loadInformation();
 
-function insertItem(item, index){
-    const tr = document.createElement('tr'); //criação dos td com as informações
-    tr.innerHTML = 
-    `
-    <td>${item.nameUser}</td>
-    <td>${item.cpf}</td>
-    <td>${item.phone}</td>
-    <td>${item.email}</td>
-    <td>
-    <button onClick="edit(${index})"><i class="material-icons">edit</i></button>
-    </td>
-    <td>
-    <button onClick="delete(${index})"><i class="material-icons">trash</i></button>
-    </td>
-    `
-    tbody.appendChild(tr); //carregando para dentro do body para ser apresentado em tela
-}
+// function insertItem(item, index){
+//     const tr = document.createElement('tr'); //criação dos td com as informações
+//     tr.innerHTML = 
+//     `
+//     <td>${item.nameUser}</td>
+//     <td>${item.cpf}</td>
+//     <td>${item.phone}</td>
+//     <td>${item.email}</td>
+//     <td>
+//     <button onClick="edit(${index})"><i class="material-icons">edit</i></button>
+//     </td>
+//     <td>
+//     <button onClick="delete(${index})"><i class="material-icons">trash</i></button>
+//     </td>
+//     `
+//     tbody.appendChild(tr); //carregando para dentro do body para ser apresentado em tela
+// }
 
-function excludeItem(index){
-    itens.splice(index, 1) //pega o index, retira um item
-    setItensLS(); //atualiza no LS
-    loadInformation(); //carrega novamente as informações
-}
+// function excludeItem(index){
+//     itens.splice(index, 1) //pega o index, retira um item
+//     setItensLS(); //atualiza no LS
+//     loadInformation(); //carrega novamente as informações
+// }
 
-function editItem(index) {
+// function editItem(index) {
 
-    insert(true, index);
-  }
+//     insert(true, index);
+//   }
 
-function insert(edit = false, index = 0){ //vem como padrao false, caso seja edição virá true
+// function insert(edit = false, index = 0){ //vem como padrao false, caso seja edição virá true
 
-    if(edit){
-        nameUser.value = itens[index].name;
-        cpf.value = itens[index].cpf;
-        phone.value = itens[index].phone;
-        email.value = itens[index].email;
-    }else{
-        nameUser.value = '';
-        cpf.value = '';
-        phone.value = '';
-        email.value = '';
-    }
-} 
+//     if(edit){
+//         nameUser.value = itens[index].name;
+//         cpf.value = itens[index].cpf;
+//         phone.value = itens[index].phone;
+//         email.value = itens[index].email;
+//     }else{
+//         nameUser.value = '';
+//         cpf.value = '';
+//         phone.value = '';
+//         email.value = '';
+//     }
+// } 
 
-btnSubmit.onclick = e => {
+// btnSubmit.onclick = e => {
   
-    if (nameUser.value == '' || cpf.value == '' || phone.value == '' || email.value == '') {
-      return
-    } //verifica nulos e vazios
+//     if (nameUser.value == '' || cpf.value == '' || phone.value == '' || email.value == '') {
+//       return
+//     } //verifica nulos e vazios
   
-    e.preventDefault();
+//     e.preventDefault();
   
-    if (id !== undefined) { // diferente de undefined, é pq veio de edição, atualiza com novos valores ou faz o push em valores novos
-      itens[id].nameUser = nameUser.value
-      itens[id].cpf = cpf.value
-      itens[id].phone = phone.value
-      itens[id].email = email.value
-    } else {
-      itens.push({'name': nameUser.value, 'cpf': cpf.value, 'phone': phone.value, 'email': email.value})
-    }
+//     if (id !== undefined) { // diferente de undefined, é pq veio de edição, atualiza com novos valores ou faz o push em valores novos
+//       itens[id].nameUser = nameUser.value
+//       itens[id].cpf = cpf.value
+//       itens[id].phone = phone.value
+//       itens[id].email = email.value
+//     } else {
+//       itens.push({'name': nameUser.value, 'cpf': cpf.value, 'phone': phone.value, 'email': email.value})
+//     }
   
-    setItensLS() // seta os novos itens na LS  
-    loadInformation() // carrega as novas informações
-    id = undefined // id undefined para pegar novo
-  }
+//     setItensLS() // seta os novos itens na LS  
+//     loadInformation() // carrega as novas informações
+//     id = undefined // id undefined para pegar novo
+//   }
 
 function inputLength(){
     if(document.getElementById('inputName').value.length < 3){
-        document.querySelector('#btnSubmit').disabled = true;
+        document.getElementById('#btnSubmit').disabled = true;
     }else{
-        document.querySelector('#btnSubmit').disabled = false;  
+        document.getElementById('#btnSubmit').disabled = false;  
+    }
+}
+
+
+function emptyFields(){
+    const nameInput = document.getElementById('inputName').value;
+    const cpf = document.getElementById('inputCpf').value;
+    const phone = document.getElementById('inputPhone').value;
+    const email = document.getElementById('inputEmail').value;
+    const btnSubmit = document.getElementById('btnSubmit');
+    const locked = document.getElementById('lockedButton').value;
+
+    if((nameInput == '' && cpf == '' && phone == '' && email == '')){
+        btnSubmit.style.backgroundColor = '#dddcdc';
+        btnSubmit.style.color ='#f6f6f6'
+        btnSubmit.disabled = true;
     }
 }
